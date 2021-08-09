9fc0b26b668bc7b045e7affbfb5aa3ba573fe753
Robert <r91palmer@gmail.com>
2021-08-09 13:25:38 -0500

    set up initial inputs for form

--- todo-app/src/Components/TodoForm.js
+++ todo-app/src/Components/TodoForm.js
@@ -2,7 +2,11 @@ import './TodoForm.css';
 
 const TodoForm = () => {
     return (
-        <h1>TodoForm</h1>
+        <div>
+            <input type="text" />
+            <input type="date" min="2018-01-01" max="2025-01-01" />
+            <button type="submit">Submit</button>
+        </div>
     );
 };
 
