5a3ec1797d8a06d318c32233dfacbc9f064703b0
Robert <r91palmer@gmail.com>
2021-08-09 13:43:06 -0500

    added state to store values from inputs

--- todo-app/src/Components/TodoForm.js
+++ todo-app/src/Components/TodoForm.js
@@ -1,11 +1,48 @@
+import React, { useState } from 'react';
 import './TodoForm.css';
 
 const TodoForm = () => {
+    const [newTitle, setNewTitle] = useState('');
+    const [newDate, setNewDate] = useState(''); 
+
+    const addNewTitle = (event) => {
+        setNewTitle(event.target.value);
+        console.log(newTitle);
+    };
+
+    const addNewDate = (event) => {
+        setNewDate(event.target.value);
+        console.log(newDate);
+    };
+
+    const submitForm = (event) => {
+        event.preventDefault();
+
+        const newTodoData = {
+            title: newTitle,
+            date: new Date(newDate)
+        }
+
+        setNewTitle('');
+        setNewDate('');
+        console.log(newTodoData);
+    };
+
     return (
         <div>
-            <input type="text" />
-            <input type="date" min="2018-01-01" max="2025-01-01" />
-            <button type="submit">Submit</button>
+            <input 
+                type="text" 
+                value={newTitle}
+                onChange={addNewTitle}
+            />
+            <input 
+                type="date" 
+                value={newDate} 
+                min="2018-01-01" 
+                max="2025-01-01"
+                onChange={addNewDate}
+            />
+            <button type="submit" onClick={submitForm}>Submit</button>
         </div>
     );
 };
