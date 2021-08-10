aa34a21873a7fb6af0abd378db3a6bfaaa096aab
Robert <r91palmer@gmail.com>
2021-08-10 09:20:05 -0500

    omitted unused initial data

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,9 +4,9 @@ import TodoForm from './Components/TodoForm';
 import TodoItems from './Components/TodoItems';
 
 const initialTodos = [
-  { id: 1, title: 'Record Ubiquity' , date: new Date(2021, 7, 6) , notes: 'I need to buy and install Pro Tools' },
-  { id: 2, title: 'Buy AxeFx III' , date: new Date(2021, 7, 6) , notes: 'Get some good clean guitar tones' },
-  { id: 3, title: 'Practice Guitar' , date: new Date(2021, 7, 6) , notes: 'Practice riffs from Aion' }
+  { id: 1, title: 'Record Ubiquity' , date: new Date(2021, 7, 6) },
+  { id: 2, title: 'Buy AxeFx III' , date: new Date(2021, 7, 6) },
+  { id: 3, title: 'Practice Guitar' , date: new Date(2021, 7, 6) }
 ];
 
 function App() {
@@ -28,7 +28,9 @@ function App() {
 
   return (
     <div className="App">
-      <TodoForm onAddTodo={addTodoHandler} />
+      <TodoForm 
+        onAddTodo={addTodoHandler} 
+      />
       <TodoItems 
         items={todos}
         deleteTodo={deleteTodoHandler}
