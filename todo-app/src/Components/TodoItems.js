5e86fa7ca58d6d2c3e84d814e3dc6688bd3262d4
Robert <r91palmer@gmail.com>
2021-08-13 09:24:07 -0500

    changed date format

--- todo-app/src/Components/TodoItems.js
+++ todo-app/src/Components/TodoItems.js
@@ -9,7 +9,7 @@ const TodoItems = (props) => {
                 <ul key={item.id}>
                     <button onClick={() => props.updateTodo(item.id)}>Update Title</button>
                     <h3>{item.title}</h3>
-                    <p>{item.date.toLocaleString('en-US')}</p>
+                    <p>{`${item.date.getMonth()}-${item.date.getDay()}-${item.date.getFullYear()}`}</p>
                     <button onClick={() => props.deleteTodo(item.id)}>Delete</button>
                 </ul>
             )
